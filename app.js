8339d1119c8d33d58d190516249b60ae9d982274
karanbadhwar <karanbadhwar07@gmail.com>
2022-03-31 22:49:52 -0400

    basic css hover and images

--- app.js
+++ app.js
@@ -20,7 +20,19 @@ const router = require('./routes/index');
 app.use('/', router);
 
 
+app.use((req,res,next) =>{
+    const err = new Error();
+    err.status = 404;
+    err.message= ['Lost Your Way Eh?', 'Sorry mate, This page does not exist, let us guide you the way back to Home Page!!']
+    res.render('page-not-found', {err})
+});
 
+app.use((err,req,res,next) =>{
+    res.status(err.status || 500);
+    err.status = 500
+    err.message = ['Server Error', 'We appologize for the inconvenience, We are working on it.']
+    res.render('error', {err})
+});
 
 app.listen(3000, () => {
     console.log('App is Listening at port:3000!!');
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const {projects} = require('../data.json');
 
 
-
 router.use('/home', (req,res,next) => {
     res.render('index', {projects});
 
@@ -18,6 +17,14 @@ router.use('/about', (req,res,next) => {
 router.use('/projects/:id', (req,res,next) => {
     const id = req.params.id;
     const projectId = projects[id];
-    res.render('project', {projectId});
+    if(projectId){
+        res.render('project', {projectId});
+    }else {
+        const err = new Error();
+        err.status = 404;
+        err.message= ['Lost Your Way Eh?', 'Sorry mate, This page does not exist, let us guide you the way back to Home Page!!']
+        res.status(err.status);
+        res.render('page-not-found', {err})
+    }
 });
 module.exports = router;
